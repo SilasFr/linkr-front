fe8a11321381d5bfdcad43a36ea9502071807580
Matheus <morais.matheus.web@gmail.com>
2022-03-22 19:57:28 -0300

    feat: login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import SignIn from "./pages/sign-in";
 import SignUp from "./pages/sign-up";
+
+import UserContext from "./contexts/userContext";
+import { useState } from "react";
+
 function App() {
+  const [userData, setUserData] = useState({
+    name: "",
+    token: "",
+    profilePic: "",
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 

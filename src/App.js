7cf4b63cfad8bf9ea3354e355a113d13f59bca3e
Silas <silas23.fr@gmail.com>
2022-03-25 12:03:50 -0300

    docs: create timeline context

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import SignUp from "./pages/sign-up";
 import Timeline from "./pages/timeline";
 import Home from "./pages/home";
 import { UserProvider } from "./contexts/userContext";
+import TimelineContext from "./contexts/timelineContext";
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/timeline" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <TimelineContext>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/timeline" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </TimelineContext>
     </UserProvider>
   );
 }

05d6e423067ae913974d1d4cfbc786c8a797bc8d
Matheus <morais.matheus.web@gmail.com>
2022-03-31 14:14:23 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 /* const BASE_URL = "https://linkr-back.herokuapp.com"; */
+const BASE_URL = "http://localhost:3000";
 
 function createConfig(token) {
   return {
@@ -10,11 +11,11 @@ function createConfig(token) {
   };
 }
 async function createUser(user) {
-  await axios.post(`${process.env.BASE_URL}/sign-up`, user);
+  await axios.post(`${BASE_URL}/sign-up`, user);
 }
 
 async function login(user) {
-  const response = await axios.post(`${process.env.BASE_URL}/`, user);
+  const response = await axios.post(`${BASE_URL}/`, user);
   return response.data;
 }
 
@@ -24,39 +25,30 @@ async function loadPosts(token, offset) {
     offsetQuery = `?offset=${offset}`;
   }
   const config = createConfig(token);
-  const result = await axios.get(
-    `${process.env.BASE_URL}/timeline${offsetQuery}`,
-    config
-  );
+  const result = await axios.get(`${BASE_URL}/timeline${offsetQuery}`, config);
   return result;
 }
 
 async function loadPostsByHashtag(hashtag, token) {
   const config = createConfig(token);
-  const result = await axios.get(
-    `${process.env.BASE_URL}/hashtag/${hashtag}`,
-    config
-  );
+  const result = await axios.get(`${BASE_URL}/hashtag/${hashtag}`, config);
   return result;
 }
 async function loadPostsByUserId(token, userId) {
   const config = createConfig(token);
-  const result = await axios.get(
-    `${process.env.BASE_URL}/timeline/${userId}`,
-    config
-  );
+  const result = await axios.get(`${BASE_URL}/timeline/${userId}`, config);
   return result;
 }
 
 async function logout(token) {
-  const response = await axios.post(`${process.env.BASE_URL}/logout`, token);
+  const response = await axios.post(`${BASE_URL}/logout`, token);
   return response.data;
 }
 
 async function newPost(postData, token) {
   const config = createConfig(token);
   const newPostResponse = await axios.post(
-    `${process.env.BASE_URL}/posts/new`,
+    `${BASE_URL}/posts/new`,
     postData,
     config
   );
@@ -68,27 +60,24 @@ async function postHashtags(hashtagsArray, token) {
     hashtags: hashtagsArray,
     token: token,
   };
-  const response = await axios.post(
-    `${process.env.BASE_URL}/hashtag`,
-    hashtags
-  );
+  const response = await axios.post(`${BASE_URL}/hashtag`, hashtags);
   return response;
 }
 
 async function getHashtags() {
-  const result = await axios.get(`${process.env.BASE_URL}/hashtag`);
+  const result = await axios.get(`${BASE_URL}/hashtag`);
   return result.data;
 }
 
 async function validateSession(token) {
   const config = createConfig(token);
-  const response = await axios.get(`${process.env.BASE_URL}/`, config);
+  const response = await axios.get(`${BASE_URL}/`, config);
   return response.data;
 }
 async function searchUser(token, query) {
   const config = createConfig(token);
   const response = await axios.get(
-    `${process.env.BASE_URL}/timeline/users?user=${query}`,
+    `${BASE_URL}/timeline/users?user=${query}`,
     config
   );
   return response;
@@ -96,53 +85,36 @@ async function searchUser(token, query) {
 
 async function deletePost(id, token) {
   const config = createConfig(token);
-  const response = await axios.delete(
-    `${process.env.BASE_URL}/posts/${id}`,
-    config
-  );
+  const response = await axios.delete(`${BASE_URL}/posts/${id}`, config);
   return response;
 }
 async function editPost(postData, id, token) {
   const config = createConfig(token);
-  const response = await axios.put(
-    `${process.env.BASE_URL}/posts/${id}`,
-    postData,
-    config
-  );
+  const response = await axios.put(`${BASE_URL}/posts/${id}`, postData, config);
   return response;
 }
 async function loadPostById(id, token) {
   const config = createConfig(token);
-  const response = await axios.get(
-    `${process.env.BASE_URL}/posts/${id}`,
-    config
-  );
+  const response = await axios.get(`${BASE_URL}/posts/${id}`, config);
   return response;
 }
 
 async function getLikesByPostId(id, token) {
   const config = createConfig(token);
-  const response = await axios.get(
-    `${process.env.BASE_URL}/likes/${id}`,
-    config
-  );
+  const response = await axios.get(`${BASE_URL}/likes/${id}`, config);
   return response.data;
 }
 
 async function likePost(id, token) {
   const config = createConfig(token);
-  const response = await axios.post(
-    `${process.env.BASE_URL}/posts/${id}/like`,
-    {},
-    config
-  );
+  const response = await axios.post(`${BASE_URL}/posts/${id}/like`, {}, config);
   return response;
 }
 
 async function dislikePost(id, token) {
   const config = createConfig(token);
   const response = await axios.post(
-    `${process.env.BASE_URL}/posts/${id}/dislike`,
+    `${BASE_URL}/posts/${id}/dislike`,
     {},
     config
   );
